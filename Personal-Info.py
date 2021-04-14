25ae020fe3c9cc1b982c04d9358203bc53b8c7a4
HeyJustinG <82501655+HeyJustinG@users.noreply.github.com>
2021-04-14 16:18:53 +0800

    Add files via upload

---
+++ Personal-Info.py
@@ -0,0 +1,13 @@
+firstn = "Justin Rey"
+lastn = "Gultiano"
+age = 19
+address = "Alfonso Village Alapan 1B Imus, Cavite"
+CYS = "BIT21"
+space = " "
+
+
+
+print(firstn + space + lastn)
+print(CYS)
+print(str(age))
+print(address)
